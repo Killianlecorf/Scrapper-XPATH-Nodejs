a2335133e10f21d8f6a0aea53a63a1244e33af68
killian-lecorf <killianlecorf@outlook.fr>
2022-12-06 21:32:46 +0100

    map links

--- scrapper/src/index.js
+++ scrapper/src/index.js
@@ -1,20 +1,43 @@
 const xpath = require('xpath-html');
 
-const getDocument = async () => {
+const getDocument = async (pageNumber) => {
     try {
-        const response = await fetch('https://liamcariou.fr/#hero')
-        const page = await response.text();
-        const document = xpath.fromPageSource(page)
-        return document;
+            const response = await fetch('http://vps-a47222b1.vps.ovh.net:8484/Product/page/' + pageNumber )
+            const page = await response.text();
+            const document = xpath.fromPageSource(page)
+            return document; 
     } catch (error) {
         console.log(error);
     }
 }
 
-const getElement = async () => {
-    const document = await getDocument();
-    const element = document.findElement('//h1[@class = "text-uppercase"]')?.getText()
-    console.log(element);
+const getPages = async () => {
+    let pages = []
+    for (let pageNumber = 1; pageNumber <= 8; pageNumber++) {
+        const document = await getDocument(pageNumber)  
+        pages.push(document)     
+    }
+    return pages
 }
 
-getElement();
+const getElement = async () => {
+    const pages = await getPages();
+    const document = await getDocument(1);
+    const element = document.findElement("//a[@class='btn btn-primary'][starts-with(@href, '/product/')]")?.getText()
+    return element;
+}
+
+const getPagesLinks = async () => {
+    const pages = await getPages();
+    const pagesButtons = pages.map( page => page.findElements("//a[@class='btn btn-primary'][starts-with(@href, '/product/')]") )
+    
+    let pagesLinks = [];
+    for (const pageButtons of pagesButtons) {
+        const links = pageButtons.map( button => button.getAttribute('href'))
+        pagesLinks = [...pagesLinks, ...links]
+    }
+    console.log(pagesLinks);
+    return pagesLinks
+}
+
+getPagesLinks()
