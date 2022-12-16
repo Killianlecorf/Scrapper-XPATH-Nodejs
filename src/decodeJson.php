d785a325e29cc200559aa8357eda543fad69a2d1
killian-lecorf <killianlecorf@outlook.fr>
2022-12-16 17:31:10 +0100

    add page product & responsive

--- src/decodeJson.php
+++ src/decodeJson.php
@@ -13,20 +13,21 @@ function viewArticle() {
     $listData = decodeJson();
     $products = $listData['produits'];
     $htmlProducts = "";
+    $index = -1 ;
     foreach ($products as $product) {
-        $index = '' ;
+        $index = ++$index ;
         $image = $product['img'];
         $title = $product['title'];
         $price = $product['price'];
 
         $htmlProducts = $htmlProducts . 
-        "<a href=''><div class='article-product'>
+        "<a class='link-product' href='pageProduct.php?page=$index'><div class='article-product'>
             <div class='img-Article-product'>
                 <img class='img-product' src= '$image'  alt=''>
             </div>
             <div class='info-Article-product'>
                 <div class='title-area'>
-                    <p class='title-product'>$title </p>
+                    <p class='title-product'>$title</p>
                 </div>
                 <div class='desc-area'>
                     <p class='desc-product'>$price  </p>
@@ -36,4 +37,5 @@ function viewArticle() {
         }
     return $htmlProducts ;
 };
+
 ?>
